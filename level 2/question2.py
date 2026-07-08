9c927aec0016693dfa10bed8fa92c7ffe4d3fcab
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-08 11:35:59 +0530

    question2.py

--- level 2/question2.py
+++ level 2/question2.py
@@ -1,2 +1,18 @@
 # create a function that takes a string as input and 'count' the 'Vowels'.
+def vowels(text):
+    vowels_store=""
+    vowels_count=0
+    for check in text:
+        if check in "aeiou":
+            vowels_store+=check
+            vowels_count+=1
+    print("vowels_word=",vowels_store)  
+    print("count=",vowels_count)      
 
+word=input("enter word=")
+vowels(word)
+
+# o\p
+# enter word=education
+# vowels_word= euaio
+# count= 5
