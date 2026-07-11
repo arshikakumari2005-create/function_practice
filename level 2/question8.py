45c0c549b14605c6e593591d99b40e6395477476
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-11 15:54:01 +0530

    question1.py

--- level 2/question8.py
+++
@@ -1,8 +0,0 @@
-# create a function that takes celsius  as input and convert into fahrenheit.
-def celsiuc_into_fahrenheit(celsius):
-
-    fahrenheit=(celsius * 9/5)+32
-    print("Fahrenheit=",fahrenheit)
-
-celsius=float(input("enter temperature in celsius="))
-celsiuc_into_fahrenheit(celsius)
