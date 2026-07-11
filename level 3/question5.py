3d1f8d995934785da6bb117735a0bbf24d1c7a68
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-11 15:01:59 +0530

    question5.py

---
+++ level 3/question5.py
@@ -0,0 +1,7 @@
+# find the largest number in the list.
+def largest_num(user_list):
+    return max(user_list)
+
+user_list=[12,34,53,23,65,47,89,105,28]
+result =largest_num(user_list)
+print(f"{user_list}\nlargest num={result}") 
