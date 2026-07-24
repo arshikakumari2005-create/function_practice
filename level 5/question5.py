265fe33304df324b976e397ea2e61cd898e8acbf
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-24 21:00:58 +0530

    question5.py

--- level 5/question5.py
+++ level 5/question5.py
@@ -1,1 +1,11 @@
-# 
+# search student marks
+student={"arshika":85.80,
+         "rohan":78.0,
+         "aman":56.0,
+         "raman":79.60,
+         "suman":81.0,}
+user=input("enter name for search marks=").lower()
+for key,value in student.items():
+    if key == user:
+        print(f"student name ={key} and marks ={value}")            
+        
